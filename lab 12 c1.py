16dee6a945039ed1571081045c3fa9e013c9f965
MuhammadShaarif <gamesofficial705@gmail.com>
2025-12-14 23:22:42 +0500

    Fixed required program code according to question

--- lab 12 c1.py
+++ lab 12 c1.py
@@ -1,11 +1,12 @@
-def search(datalist, userSearch):
-    for i in datalist:
-        if userSearch == i:
-            print("Search Result found!")
-            break
-    else:
-        print("Not found")
-datalist = [1, 2, 3, 4, 5]
+def linear_search(datalist, element):
+    for index in range(len(datalist)):
+        if datalist[index] == element:
+            return index
+    return "not found"
 
+
+datalist = [1, 2, 3, 4, 5]
 user_search = int(input("Enter a value to search: "))
-search(datalist, user_search)
+
+result = linear_search(datalist, user_search)
+print(result)
